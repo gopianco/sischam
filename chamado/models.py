094df449660dc0bfd86c944c3875b95ad33fa648
Giovane Pianco <gopianco@hotmail.com>
2019-12-26 16:01:38 -0300

    alteracao de consulta

--- chamado/models.py
+++ chamado/models.py
@@ -5,24 +5,26 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Cliente(User):
-    endereco = models.CharField(max_length=200)
+class Equipamento(models.Model):
+    id_equipamento = models.AutoField(primary_key=True)
+    descricao = models.TextField(max_length=200, blank=False, null=False)
+    local = models.TextField(max_length= 100)
 
+    def __str__(self):
+        return self.descricao
+
+class Cliente(User):
+    id_cliente = models.AutoField(primary_key=True)
+    endereco = models.CharField(max_length=200)
     class Meta:
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
 
 class Analista(User):
-    is_staff = True
+    id_analista = models.AutoField(primary_key=True)
     class Meta:
         verbose_name = 'Analista'
         verbose_name_plural = 'Analistas'
-
-
-class Equipamento(models.Model):
-    id_equipamento = models.AutoField(primary_key=True)
-    descricao = models.TextField(max_length=200, blank=False, null=False)
-    local = models.TextField(max_length= 100)
 
 
 class Chamado(models.Model):
@@ -43,11 +45,11 @@ class Chamado(models.Model):
     id_chamado = models.AutoField(primary_key=True)
     tipo_chamado = models.CharField(max_length=20,choices=TIPO_DE_CHAMADO, default='Incidente')
     status= models.CharField(max_length=20, choices=STATUS_DE_CHAMADO, default='Aberto')
-    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
-    analista = models.OneToOneField(Analista, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='chamados')
+    analista = models.ForeignKey(Analista, on_delete=models.CASCADE, related_name='chamados')
     equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE)
     descricao = models.TextField()
-    solucao = models.TextField()
+    solucao = models.TextField(blank=True, null=True)
     #chat = models.ForeignKey()
     data_abertura = models.DateField(default=timezone.now)
     data_alteracao = models.DateField(default=timezone.now)
@@ -64,5 +66,4 @@ class Chamado(models.Model):
 
     def __str__(self):
         return self.titulo_chamado
-
 
--- chamado/views.py
+++ chamado/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Chamado
 
 def chamados_list(request):
-    chamados = Chamado.objects.all().order_by('data_abertura')
+    chamados = Chamado.objects.filter(cliente=1).order_by('status')
     return render(request, 'chamado/chamados_list.html', {'chamados': chamados})
